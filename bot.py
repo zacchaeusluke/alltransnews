c8a50a3672a4a9cb5a4a2d51493adeafb9ef44da
snape@zacchaeus.co.uk <zacsnape@gmail.com>
2018-01-17 09:28:08 +0000

    First commit to github

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ from bs4 import BeautifulSoup
 import urllib2, feedparser, urlparse, os.path
 import cPickle as pickle
 import calendar, hashlib, time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # ====== Individual bot configuration ==========================
 bot_username = 'alltransnews'
@@ -35,28 +35,13 @@ logfile_name = bot_username + ".log"
 
 # ==============================================================
 
-cache_items = []
-
-#get cache from file
-if os.path.exists("cache.p"):
-    cache = pickle.load( open( "cache.p", "rb" ) )
-    #clean out old items from cache
-    for item in cache:
-        if (item["added"] - datetime.now()).total_seconds() > 4320:
-            cache.remove(item)
-        else:
-            cache_items.append(item["hash"])
-else:
-    cache = []
-    pickle.dump(cache, open("cache.p", "wb"))
-
 def parse_feed():
     global last_accessed
     tweet_list = []
-    feed_data = feedparser.parse('https://news.google.com/news?q=transgender&scoring=n&output=rss')
+    feed_data = feedparser.parse('https://www.google.co.uk/alerts/feeds/17250414904985055934/9902643025203101686')
     for entry in feed_data.entries:
-        if hashlib.md5(str(entry.id)).hexdigest() not in cache_items:
-            title = entry.title #get title of article
+        if entry.id not in cache_items:
+            title = BeautifulSoup(entry.title, "html.parser").get_text().encode("utf-8") #get title of article
             out = urlparse.urlparse(str(entry.link)) #parse link url
             q = urlparse.parse_qs(out[4]) #get query parameters
             direct_link = q['url'][0] #get the direct url to article
@@ -67,8 +52,8 @@ def parse_feed():
 def create_tweet(data):
     """Create the text of the tweet you want to send."""
     title = data["title"]
-    if len(title) > 117: #check tweet isn't too long
-        title = title[:115]+'..'
+    if len(title) > 115: #check tweet isn't too long
+        title = title[:113]+'..'
     text = title+" "+data["link"]
     return text
 
@@ -78,7 +63,7 @@ def tweet(text, id_string):
     auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
     auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
     api = tweepy.API(auth, wait_on_rate_limit=True)
-
+    
     # Send the tweet and log success or failure
     try:
         api.update_status(text)
@@ -86,14 +71,31 @@ def tweet(text, id_string):
         print str(e.message[0]['message'])
     else:
         print "Tweeted: " + text
-        cache.append({'hash': hashlib.md5(id_string).hexdigest(), 'added': datetime.now()}) #add hash to cache
+        cache.append({'id': id_string, 'added': datetime.now()}) #add hash to cache
+        time.sleep(30) #wait between sending tweets so we're not flooding
 
 if __name__ == "__main__":
+    cache_items = []
+
+    #get cache from file
+    if os.path.exists("cache.p"):
+        cache = pickle.load( open( "cache.p", "rb" ) )
+        #clean out old items from cache
+        for item in cache:
+            if item["added"] < (datetime.now() - timedelta(days=30)):
+                cache.remove(item)
+                print "Removed: " + str(item["added"])
+            else:
+                cache_items.append(item["id"])
+    else:
+        cache = []
+        pickle.dump(cache, open("cache.p", "wb"))
+
+    #parse the rss feed
     tweets = parse_feed()
+    #for each tweet, create the tweet and send it
     for tweet_data in tweets:
         tweet_text = create_tweet(tweet_data)
         tweet(tweet_text, tweet_data["id"])
-        time.sleep(30) #wait between sending tweets so we're not flooding
 
-print cache
 pickle.dump(cache, open("cache.p", "wb"))
